1e784260c537e1bd37cec41d4ca1d460010c2b1f
Mildophin <viricel.milan@gmail.com>
2021-04-12 16:29:20 +0200

    Analyse d'une page livre du site
    1) Récupération de:
    - la note sur 5
    - l'adresse de l'image
    
    2) Le programme crée un répertoire Photos et télécharge les photos des livres dedans
    
    3) Affiche les données de chaque livre sur l'ensemble des pages du site
    
    Reste à faire: Ecriture des fichiers CSV

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 """"
-review_rating
 image_url
 
+print(links)
 
-ratingbloc = soup.find(class_="product_main")
-if soup.find(class_="star-rating One") != "None":
-    rating = "One"
-elif soup.find(class_="star-rating Two") != "None":
-    rating = "Two"
-elif soup.find(class_="star-rating Three") != "None":
-    rating = "Three"
-elif soup.find(class_="star-rating Four") != "None":
-    rating = "Four"
+with open('urls.csv', 'w') as file:
+    for link in links:
+        file.write(link + '\n')
 
+"""
 
 links = []
+"""
+url="http://books.toscrape.com/"
 
-for i in range(10):
+pageaccueil = requests.get(url)
+soup = BeautifulSoup(pageaccueil.text, features="html.parser")
+listing= int(soup.find('li', {'class': 'current'}).string.strip()[10:])
+"""
+
+for i in range(1, 3):
     print("Page " + str(i))
 
-    url="http://books.toscrape.com/catalogue/page-" + str(i) + ".html"
+    url = "http://books.toscrape.com/catalogue/page-" + str(i) + ".html"
 
     response = requests.get(url)
 
@@ -34,30 +37,44 @@ for i in range(10):
             link = a['href']
             links.append("http://books.toscrape.com/catalogue/" + link)
 
-print(links)
+for link in links:
 
-with open('urls.csv', 'w') as file:
-    for link in links:
-        file.write(link + '\n')
+    responsebook = requests.get(link)
 
-"""
+    if responsebook.ok:
+        soup = BeautifulSoup(responsebook.text, features="html.parser")
+        category = soup.findAll('li')
+        category = category[2].find('a').string
+        title = soup.findAll('h1')
+        title = title[0].string
+        image_url = soup.find('div', {'class': 'item active'}).find('img')
+        image_url = "http://books.toscrape.com/" + image_url['src'][6:]
+        if not os.path.exists("Photos"):
+            os.makedirs("Photos")
+        path = os.path.dirname(__file__)
+        filename = image_url.split('/')[-1]
+        completefilename = path + "/Photos/" + filename
+        r = requests.get(image_url, allow_redirects=True)
+        open(completefilename, 'wb').write(r.content)
 
-url="http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
-
-response = requests.get(url)
-
-if response.ok:
-    soup = BeautifulSoup(response.text, features="html.parser")
-    category = soup.findAll('li')
-    category = category[2].find('a').string
-    title = soup.findAll('h1')
-    title = title[0].string
-    description = soup.findAll('p')
-    description = description[3].string
-    tds = soup.findAll('td')
-    upc = tds[0].string
-    pet = "".join([x for x in tds[2].string if ord(x) < 128])
-    pit = "".join([x for x in tds[3].string if ord(x) < 128])
-    numberavailable = "".join(c for c in tds[5].string if c.isdigit())
-'1234566890'
-print(url, upc, pet, pit, numberavailable, category, title, description)
+        if soup.find('div', {'class': 'product_main'}).find('p', {'class': 'star-rating One'}):
+            rating = "One"
+        elif soup.find('div', {'class': 'product_main'}).find('p', {'class': 'star-rating Two'}):
+            rating = "Two"
+        elif soup.find('div', {'class': 'product_main'}).find('p', {'class': 'star-rating Three'}):
+            rating = "Three"
+        elif soup.find('div', {'class': 'product_main'}).find('p', {'class': 'star-rating Four'}):
+            rating = "Four"
+        elif soup.find('div', {'class': 'product_main'}).find('p', {'class': 'star-rating Five'}):
+            rating = "Five"
+        else:
+            print("Inconnu")
+        description = soup.findAll('p')
+        description = description[3].string
+        tds = soup.findAll('td')
+        upc = tds[0].string
+        pet = "".join([x for x in tds[2].string if ord(x) < 128])
+        pit = "".join([x for x in tds[3].string if ord(x) < 128])
+        numberavailable = "".join(c for c in tds[5].string if c.isdigit())
+    '1234566890'
+    print(link, image_url, upc, pet, pit, numberavailable, category, title, rating, description)
